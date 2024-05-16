504eb6e2a8b179c091e4ab222cf4dc8b82405dd3
Aravind-TheTech <160785236+Aravind-TheTech@users.noreply.github.com>
2024-05-16 09:43:27 +0530

    Update doc_push.sh

--- doc_push.sh
+++ doc_push.sh
@@ -13,10 +13,10 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Tag and push the Docker image to dev repository with dev tag
     docker tag "$DOCKER_IMAGE" "$DEV_REPO:$DEV_TAG"
     docker push "$DEV_REPO:$DEV_TAG"
-fi
+
 
 # Check if the current branch is 'master' (assuming the master branch is where merges from dev occur)
-if [ "$GIT_BRANCH" == "origin/master" ]; then
+elif [ "$GIT_BRANCH" == "origin/master" ]; then
     
     # Tag and push the same Docker image to prod repository with prod tag
     docker tag "$DOCKER_IMAGE" "$PROD_REPO:$PROD_TAG"
