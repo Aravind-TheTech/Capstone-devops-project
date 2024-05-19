253df66ada141070bd3dc7404f042b795c2dbefb
Aravind-TheTech <160785236+Aravind-TheTech@users.noreply.github.com>
2024-05-19 22:10:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOCKER_IMAGE=webapp
-docker build -t ${DOCKER_IMAGE}:latest /home/ubuntu/Capstone-devops-project/webserver/static
+docker build -t ${DOCKER_IMAGE}:latest /home/ubuntu/Capstone-devops-project/webserver/static .
  
