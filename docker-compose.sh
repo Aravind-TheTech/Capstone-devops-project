25612493bbe7536b8b60f21ccf6412c57bd3cb1c
Aravind-TheTech <160785236+Aravind-TheTech@users.noreply.github.com>
2024-05-20 11:49:25 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
+if [[ $GIT_BRANCH == "origin/master" ]]; then
     
     # Tag and push the Docker image to dev repository with dev tag
    sudo docker-compose up -d
